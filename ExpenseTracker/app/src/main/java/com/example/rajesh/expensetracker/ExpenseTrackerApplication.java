fd91c8dd797b170143e900fd3cba237f1b45c935
rajesh <rajeshkhadka@lftechnology.com>
2016-03-23 16:23:22 +0545

    add navigation drawer

---
+++ ExpenseTracker/app/src/main/java/com/example/rajesh/expensetracker/ExpenseTrackerApplication.java
@@ -0,0 +1,14 @@
+package com.example.rajesh.expensetracker;
+
+import android.app.Application;
+
+/**
+ * Created by rajesh on 3/23/16.
+ */
+public class ExpenseTrackerApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
