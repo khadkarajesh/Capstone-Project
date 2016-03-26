b4809af33f6d27da53d1f2218fdf83a91d9a6a52
rajesh <rajeshkhadka@lftechnology.com>
2016-03-26 16:50:20 +0545

    add card view

--- ExpenseTracker/app/src/main/java/com/example/rajesh/expensetracker/DashboardActivity.java
+++ ExpenseTracker/app/src/main/java/com/example/rajesh/expensetracker/DashboardActivity.java
@@ -6,7 +6,6 @@ import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -15,8 +14,6 @@ import com.example.rajesh.expensetracker.dashboard.DashBoardFragment;
 
 public class DashboardActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-
-    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
